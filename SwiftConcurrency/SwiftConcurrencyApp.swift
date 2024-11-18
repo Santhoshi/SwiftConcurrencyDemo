d374c1309f68838a30b43f5fda5cdb5e97598312
Santhoshi <santhoshi2005@gmail.com>
2024-11-18 09:03:11 -0800

    Initial Commit

---
+++ SwiftConcurrency/SwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyApp.swift
+//  SwiftConcurrency
+//
+//  Created by Santhoshi Guduru on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
