029cc30209eb46a57bdeb1ccb294764aef2681ae
San <santhoshi2005@gmail.com>
2024-11-25 17:50:47 -0800

    Do Catch bootcamp

--- SwiftConcurrency/DoCatchTryThrows.swift
+++ SwiftConcurrency/DoCatchTryThrows.swift
@@ -7,9 +7,53 @@
 
 import SwiftUI
 
+class DoCatchTryThrowsDataManager {
+    
+    let isActive: Bool = true
+    
+    func getTitle() throws-> String {
+        if isActive{
+            return "New title!!"
+        }else {
+            throw URLError(.badURL)
+        }
+    }
+    func getDifferentTitle() throws -> String{
+        if isActive{
+            return "Different title"
+        }else {
+            throw URLError(.badServerResponse)
+        }
+    }
+}
+class DoCatchTryThrowsViewModel: ObservableObject{
+    @Published var text: String = "Start"
+    let manager = DoCatchTryThrowsDataManager()
+    func fetchTitle(){
+        do {
+            //optional try
+            let result = try? manager.getTitle()
+            if let newTitle = result{
+                self.text = newTitle
+
+            }
+            
+            let differentResult = try manager.getDifferentTitle()
+            self.text = differentResult
+        }catch {
+            self.text = error.localizedDescription
+        }
+    }
+}
 struct DoCatchTryThrows: View {
+    @StateObject var viewModel = DoCatchTryThrowsViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(viewModel.text)
+            .frame(width:300, height: 300)
+            .background(Color.yellow)
+            .onTapGesture {
+                viewModel.fetchTitle()
+            }
     }
 }
 
--- SwiftConcurrency/DownloadImageWithAsync.swift
+++ SwiftConcurrency/DownloadImageWithAsync.swift
@@ -6,10 +6,96 @@
 //
 
 import SwiftUI
+import Combine
+
+class DownloadImageWithAsyncImageLoader {
+
+    let url = URL(string: "https://picsum.photos/200")!
+    // Function to handle response
+    func handleResponse(data: Data?, response: URLResponse?) -> UIImage?{
+        guard let data = data,
+              let imageFromData = UIImage(data: data),
+              let response = response as? HTTPURLResponse,
+              response.statusCode >= 200 && response.statusCode < 300 else{
+            return nil
+        }
+        return imageFromData
+    }
+    //Download With @escaping
+    func downloadWithEscaping(completionHandler: @escaping(_ image: UIImage?, _ error: Error?) -> ()){
+        URLSession.shared.dataTask(with: url) {[weak self] data, response, error in
+            let imageFromData = self?.handleResponse(data: data, response: response)
+            completionHandler(imageFromData,nil)
+        }
+        .resume()
+    }
+    //Download with Combine
+    func downloadWithCombine() -> AnyPublisher<UIImage?, Error>{
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(handleResponse)
+            .mapError({ $0 })
+            .eraseToAnyPublisher()
+    }
+    // Download with Async/Await
+    func downloadWithAsync() async throws -> UIImage? {
+        do{
+            let (data, response) = try await (URLSession.shared.data(from: url, delegate: nil))
+            return handleResponse(data: data, response: response)
+        }catch{
+            throw error
+        }
+    }
+}
+
+
+class DownloadImageWithAsyncViewModel: ObservableObject{
+    @Published var image: UIImage? = nil
+    let loader = DownloadImageWithAsyncImageLoader()
+    var cancellables = Set<AnyCancellable>()
+    
+    //Fetch Image from the Image Loader
+    func fetchImage() async{
+        /*
+//        loader.downloadWithEscaping {[weak self] image, error in
+//            DispatchQueue.main.async {
+//                self?.image = image
+//            }
+//        }
+        //fetch download with Combine
+        loader.downloadWithCombine()
+            .receive(on: DispatchQueue.main) // receive on main thread
+            .sink { _ in
+                
+                } receiveValue: { [weak self] image in
+                    self?.image = image
+            }
+            .store(in: &cancellables)
+         */
+        
+        let image = try? await loader.downloadWithAsync()
+        await MainActor.run {
+            self.image = image
+        }
+    }
+     
+}
 
 struct DownloadImageWithAsync: View {
+    @StateObject var viewModel = DownloadImageWithAsyncViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            if let image = viewModel.image{
+                Image(uiImage:image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250,height: 250)
+            }
+        }
+        .onAppear{
+            Task{
+                await viewModel.fetchImage()
+            }
+        }
     }
 }
 
